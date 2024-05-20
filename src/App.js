dd4297e5730cd327baea57e437a8fbfac1ec1f8c
Rahul Raj <rahulraj18670@gmail.com>
2024-05-21 00:58:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  function fetchmoviehandler() {
+    fetch("https://swapi.dev/api/films")
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchmoviehandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
